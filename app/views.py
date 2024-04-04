1914104368fced91da732553a85b97960179c349
nandithasn <nandithasn@christcollegemalur.com>
2024-04-04 17:33:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ammu(request):
+    return render(request,'ammu.html')
+def ramu(request):
+    return render(request,'ramu.html')
